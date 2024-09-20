b510d72f6f536a66c26038a0ad59dd3661b5abbc
Michael <bjmmtin@gmail.com>
2024-09-20 16:53:06 -0400

    update personal-data and contact-data

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'bjmmtin@gmail.com',
+    phone: '+16475434894',
+    address: '310 S Michigan Ave, Chicago, Illinois ',
+    github: 'https://github.com/sven3270350',
+    facebook: 'https://www.facebook.com/',
+    linkedIn: 'https://www.linkedin.com/',
+    twitter: 'https://twitter.com/',
+    stackOverflow: 'https://stackoverflow.com/',
+    devUsername: "michel_1211"
 }
