99675ffd8f741a67bf83f0048559cac618112d9a
Michael <bjmmtin@gmail.com>
2024-09-21 19:51:36 -0400

    update the project

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -49,7 +49,7 @@ function ProjectCard({ project }) {
                 </React.Fragment>
               ))
             }
-            <span className="text-gray-400">{"],"}</span>
+            <span className="text-gray-400">{"'],"}</span>
           </div>
           <div>
             <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,19 +6,19 @@ import travel from '/public/image/travel.jpg';
 export const projectsData = [
     {
         id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
+        name: 'Realtor App',
+        description: "This  is a comprehensive real estate platform that utilizes React.js, Next.js, Node.js, AWS, MongoDB, and Tailwind CSS to deliver a seamless user experience. The site features an interactive property search tool, allowing users to browse listings with detailed information, including images, descriptions, and pricing. Backend services are powered by Node.js and AWS, ensuring scalability and efficient data handling. The frontend is built with React.js and Next.js, providing a dynamic and responsive interface that enhances user engagement. Overall, this leverages cutting-edge technologies to connect buyers, sellers, and real estate professionals in an efficient and effective manner.",
+        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "ReactJS", "TypeScript", "MongoDB", "AWS", "NodeJs"],
+        role: 'Full Stack Developer',
         code: '',
         demo: '',
         image: crefin,
     },
     {
         id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
+        name: 'Manage Help App',
+        description: 'This is a user-friendly platform designed to streamline management tasks and enhance productivity. Built using React and Tailwind CSS, the site offers a sleek and modern interface that is responsive and easy to navigate. The backend is powered by Node.js and AWS, providing a robust and scalable environment for handling user data and application logic. The application enables users to manage their tasks, collaborate with team members, and track progress in real-time. Overall, the platform leverages contemporary web technologies to empower users in managing their work effectively and efficiently.',
+        tools: ["NodeJs", 'Tailwind CSS', "ReactJS", "TypeScript", "MongoDB", "AWS" ],
         role: 'Full Stack Developer',
         code: '',
         demo: '',
@@ -26,23 +26,13 @@ export const projectsData = [
     },
     {
         id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+        name: 'LeastSquares App',
+        description: 'The website is built using Next.js and Tailwind CSS. The backend is powered by GraphQL and Prisma, enabling efficient data fetching and management. LeastSquares aims to support creators by offering a range of development tools tailored to enhance their workflows. By combining cutting-edge technologies with a passion for building, the company provides a platform that fosters creativity and innovation, making it easier for users to bring their ideas to life.',
+        tools: ['NextJs', 'Tailwind CSS' ,'GraphQL', 'Prisma', 'MySQL'],
         code: '',
         role: 'Full Stack Developer',
         demo: '',
         image: realEstate,
-    },
-    {
-        id: 4,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
-        code: '',
-        demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
     }
 ];
 
